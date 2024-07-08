4bcd6cc798e82b0dbf360d509f5e87ceb88548a3
Alice (PopovVN) <vladislav_popov46@vk.com>
2024-07-08 13:00:56 +0300

    code: изменен код

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
-    int sum = a + b;
-    cin >> a >> b >> sum >> endl;
+    cout << " A + B = " << a + b << '\n' << "A - B = " << a - b << '\n';
 
 }
